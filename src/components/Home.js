1b8b98c84d67a47912257915dbf4134583ae54e6
JamaCodes <jama.avwork@gmail.com>
2021-10-05 15:03:38 -0500

    spotlight animal added to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { AnimalSpotlight } from "../components/animals/AnimalSpotlight";
+import { getRandomId } from "../modules/AnimalManager";
+import { PropsAndState } from "./PropsAndState";
 import { TodaysDate } from "./date/DateCard";
-import { PropsAndState } from "./PropsAndState/PropsAndState";
 
-export function getCurrentDate(separator=''){
+export function getCurrentDate(separator = "") {
+  let newDate = new Date();
+  let date = newDate.getDate();
+  let month = newDate.getMonth() + 1;
+  let year = newDate.getFullYear();
 
-    let newDate = new Date()
-    let date = newDate.getDate();
-    let month = newDate.getMonth() + 1;
-    let year = newDate.getFullYear();
-    
-    return `${year}${separator}${month<10?`0${month}`:`${month}`}${separator}${date}`
-    }
+  return `${year}${separator}${
+    month < 10 ? `0${month}` : `${month}`
+  }${separator}${date}`;
+}
 
+export const Home = () => {
+  const [spotlightId, setSpotlightId] = useState(0);
 
+  const refreshSpotlightAnimal = () => {
+    getRandomId().then(setSpotlightId);
+  };
 
-export const Home = () => (
+  useEffect(() => {
+    refreshSpotlightAnimal();
+  }, []);
+
+  return (
     <>
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
+      <h2>Nashville Kennels</h2>
+      <small>Loving care when you're not there.</small>
 
+      <address>
+        <div>Visit Us at the Nashville North Location</div>
+        <div>500 Puppy Way</div>
+      </address>
+      <PropsAndState yourName={"Jama"} />
+      <TodaysDate />
+
+      <div>
         <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
+          Visit Us at the Nashville North Location
+          <br />
+          500 Puppy Way
         </address>
-        <PropsAndState yourName={"Jama"}/>
-        <TodaysDate />
-
+        <h1>Animal Spotlight</h1>
+        <button onClick={refreshSpotlightAnimal}>Reload &#x27f3;</button>
+        {spotlightId && <AnimalSpotlight animalId={spotlightId} />}
+      </div>
     </>
-)
+  );
+};
